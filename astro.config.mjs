d722012534e1993b8a2fcf506e2b98d28476c9d8
Sanna Kramsi <sanna.kramsi@gmail.com>
2024-06-28 20:28:03 +0300

    Prepare for the domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wcag.sanna.ninja',
+  site: 'https://wcag.a11y.ing',
   integrations: [sitemap({
     i18n: {
       defaultLocale: 'en',
